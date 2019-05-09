7bda212078b75803d9de8e7dbd3e867ba3db1c68
Federico Hatoum <fhatoum@grassroots-tech.com>
2019-05-09 11:19:30 -0700

    Adding hapi to see if this project continues to work

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const http = require('http');
+const Hapi = require('@hapi/hapi');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const init = async () => {
+
+    const port = process.env.PORT || 3000;
+
+    const server = Hapi.server({
+        port: port,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path:'/',
+        handler: (request, h) => {
+            return 'HAPI: AZURE: Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+init();
 
-console.log("Server running at http://localhost:%d", port);
+
+// const http = require('http');
+//
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+// });
+//
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+//
+// console.log("Server running at http://localhost:%d", port);
